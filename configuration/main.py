4ad8077ba1ab1a13834fb859998d985510ab94d9
Luthiano Trarbach <lmtrarbach@msn.com>
2024-04-27 17:00:49 +0100

    Setting networking connectivity

--- configuration/main.py
+++ configuration/main.py
@@ -10,14 +10,26 @@ import time
 def test_network_resilience(net):
     info("Testing - Should all pass\n")
     # Testing with all working setup
-    net.pingAll()
+    for each in range(1, 6):
+        if each != 4:
+            to_host = net.get("host{each}")
+            result = net.get("host{each}").cmd(f"ping -c 2 host4")
+            if " 0% packet loss" in result:
+                info(f"Connectivity between host4 and host{each} is successful")
+            else:
+                info(f"Connectivity between host4 and host{each} failed")
 
-    # Drop connection
-    net.get("host4").cmd("iptables -A OUTPUT -s 10.0.0.1 -d 10.0.0.2 -j DROP")
-    net.get("host2").cmd("iptables -A OUTPUT -s 10.0.0.2 -d 10.0.0.1 -j DROP")
+    net.get("host4").cmd("iptables -A INPUT -j DROP")
 
     info("Testing - Should fail")
-    net.pingAll()
+    for each in range(1, 6):
+        if each != 4:
+            to_host = net.get("host{each}")
+            result = net.get("host{each}").cmd(f"ping -c 2 host4")
+            if " 0% packet loss" in result:
+                info(f"Connectivity between host4 and host{each} is successful")
+            else:
+                info(f"Connectivity between host4 and host{each} failed")
 
     # Reset iptables rules
     net.get("host4").cmd("iptables -D OUTPUT -s 10.0.0.1 -d 10.0.0.2 -j DROP")
@@ -51,7 +63,6 @@ def setup_topology():
     host4 = net.addHost("host4")
     host5 = net.addHost("host5")
 
-    net.addLink(s1, s2)
     net.addLink(host1, s1)
     net.addLink(host2, s1)
     net.addLink(host3, s2)
