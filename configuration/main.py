288be6565f4cad3b0f688199b5440b329465374a
Luthiano Trarbach <lmtrarbach@msn.com>
2024-04-27 17:56:45 +0100

    Setting simpler configuration

--- configuration/main.py
+++ configuration/main.py
@@ -12,14 +12,14 @@ def test_network_resilience(net):
     s1, s2 = net.get('s1'), net.get('s2')
     net.pingAll()
 
-    s1.s2_intf1.config(loss=100)
-    s2.s1_intf2.config(loss=100)
+    s1.config(loss=100)
+    s2.config(loss=100)
 
     info("Shpuld fail\n")
     net.pingAll()
 
-    s1.s2_intf1.config(loss=0)
-    s2.s1_intf2.config(loss=0)
+    s1.config(loss=0)
+    s2.config(loss=0)
 
 
 def setup_topology():
