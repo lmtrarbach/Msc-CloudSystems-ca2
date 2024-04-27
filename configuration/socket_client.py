fc509dcd33850c75e97577737a35b74e7b8e3482
Luthiano Trarbach <lmtrarbach@msn.com>
2024-04-27 20:42:29 +0100

    Setting simpler configuration

--- configuration/socket_client.py
+++ configuration/socket_client.py
@@ -1,12 +1,15 @@
 import socket
 import sys
+from mininet.log import setLogLevel, info
 
+setLogLevel("info")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = sys.argv[0]
 port = 8888
 
 s.connect((host, port))
+info("Client connected \n")
 print(s.recv(1024))
 
 s.close()
