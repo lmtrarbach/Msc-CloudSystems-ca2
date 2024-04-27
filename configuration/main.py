f34902c612e73a9670875da13736df08b8ae1ad3
Luthiano Trarbach <lmtrarbach@msn.com>
2024-04-27 17:03:58 +0100

    Setting networking connectivity

--- configuration/main.py
+++ configuration/main.py
@@ -56,7 +56,6 @@ def setup_topology():
         waitConnected=True,
     )
     s1 = net.addSwitch("s1")
-    s2 = net.addSwitch("s2")
     host1 = net.addHost("host1")
     host2 = net.addHost("host2")
     host3 = net.addHost("host3")
@@ -65,10 +64,10 @@ def setup_topology():
 
     net.addLink(host1, s1)
     net.addLink(host2, s1)
-    net.addLink(host3, s2)
+    net.addLink(host3, s1)
     net.addLink(host4, s1)
-    net.addLink(host4, s2)
-    net.addLink(host5, s2)
+    net.addLink(host4, s1)
+    net.addLink(host5, s1)
 
     net.start()
 
